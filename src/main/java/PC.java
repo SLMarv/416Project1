673aa1d52d2c1db05ea31d717cf65610783f423f
Tia Self <tiamarie.self@bsu.edu>
2024-02-01 20:04:29 -0500

    Add code under some of the placeholder comments

--- src/main/java/PC.java
+++ src/main/java/PC.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PC extends Device{
     protected PC(String deviceID, String configPath) {
         super(deviceID, configPath);
@@ -23,7 +25,21 @@ public class PC extends Device{
         public void run() {
             while(device.running){
                 //make a scanner reading terminal input and prompt user for destination and message content info
-                //if input == exitCode then make device.running = false then break
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Enter message destination: ");
+                String deviceID = scanner.nextLine();
+
+                System.out.println("Enter message content: ");
+                String messageContent = scanner.nextLine();
+
+                if (scanner.nextLine().equalsIgnoreCase("X")) {
+                    //if input == exitCode then make device.running = false then break
+                    break;
+                }
+
+                //parse outgoing port from deviceID to pass it to sendMessage()
+
                 //Otherwise create a Message and call sendMessage(message)
             }
         }
