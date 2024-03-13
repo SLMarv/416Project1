9f9f4aecf3bc823f2549918a0a36c249793e13a6
Samuel Marvin <97563317+SLMarv@users.noreply.github.com>
2024-03-13 16:30:34 -0400

    Add ability for routers to send and receive vector tables

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -7,6 +7,11 @@ public class Router extends Device{
         private final VectorTable vectorTable = new VectorTable();
         private final Map<String, Connection> nextHopMap = new HashMap<>();
 
+        public static void main(String[] args) throws IOException {
+                Router router = new Router(args[0],args[1]);
+                router.start();
+        }
+
         protected Router(String deviceID, String configPath) throws SocketException {
                 super(deviceID, configPath);
                 for(Connection port : virtualPortList){
@@ -17,10 +22,41 @@ public class Router extends Device{
 
         @Override
         void start() throws IOException {
-
+                System.out.print("");
+                broadcastTable();
+                while (running){
+                      Message message = receiveMessage();
+                      String content = message.getMessageContent();
+                      boolean tableUpdated = false;
+                      for (String entryString : content.split("\n")){
+                              if (entryString.equals("")) continue;
+                              if (vectorTable.updateTable(entryString, message.getOriginalSenderID())){
+                                      tableUpdated = true;
+                              }
+                      }
+                      if (tableUpdated) broadcastTable();
+                }
         }
 
-        public VectorTable getVectorTable() {
-                return vectorTable;
+        private void broadcastTable() {
+                for(Connection port : virtualPortList){
+                        if (!"R".equals(port.getDeviceID().substring(0,1))) continue;
+                        Message message = new Message(
+                                null,
+                                getDeviceID(),
+                                port.getDeviceID(),
+                                convertTableToString(vectorTable)
+                        );
+                        sendMessage(message, port);
+                        System.out.println(message.getMessageContent());
+                }
+        }
+
+        private String convertTableToString(VectorTable vectorTable) {
+                StringBuilder stringBuilder = new StringBuilder();
+                for (TableEntry entry : vectorTable.getTable().values()){
+                        stringBuilder.append(entry.toString()).append("\n");
+                }
+                return stringBuilder.toString();
         }
 }
